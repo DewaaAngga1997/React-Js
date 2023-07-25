75c5621fb5a2d9e4870196ff046804c81de4802b
Dewa Angga <devaangga53@@gmail.com>
2023-07-25 21:27:50 +0800

    Event Hendler

--- src/Pages/products.jsx
+++ src/Pages/products.jsx
@@ -1,35 +1,62 @@
-import CardProduct from "../components/Fragment/CardProduct";
-
+import { Fragment } from 'react';
+import CardProduct from '../components/Fragment/CardProduct';
+import Button from '../components/Elements/Button/Button';
 
 const products = [
-    {
-        id: 1,
-        image: "/images/product-1.jpg",
-        name: "Sepatu Nike Green",
-        price: "Rp 1.000.000",
-        description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam exercitationem unde inventore adipisci alias pariatur cumque, aut aliquam sunt possimus doloremque similique quam est voluptatibus facere, iste placeat enim iure.",
-    },
-    {
-        id: 2,
-        image: "/images/product-2.jpg",
-        name: "Sepatu Nike Red",
-        price: "Rp 1.500.000",
-        description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. "
-    }
-]
+  {
+    id: 1,
+    image: '/images/product-1.jpg',
+    name: 'Sepatu Nike Green',
+    price: 'Rp 1.000.000',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam exercitationem unde inventore adipisci alias pariatur cumque, aut aliquam sunt possimus doloremque similique quam est voluptatibus facere, iste placeat enim iure.',
+  },
+  {
+    id: 2,
+    image: '/images/product-2.jpg',
+    name: 'Sepatu Nike Red',
+    price: 'Rp 1.500.000',
+    description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. ',
+  },
+  {
+    id: 3,
+    image: '/images/product-1.jpg',
+    name: 'Sepatu Nike Red',
+    price: 'Rp 500.000',
+    description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. ',
+  },
+];
+
+//cara memanggil email yg sudah tersimpan saat login di local storage
+const email = localStorage.getItem('email');
+
 const ProductsPage = () => {
-    return (
-        <div className="flex justify-center py-5">
-            {products.map((product) => (
-                 <CardProduct key={product.id}>
-                 <CardProduct.Header image = {product.image} />
-                 <CardProduct.Body name ={product.name}>{product.description}</CardProduct.Body>
-                 <CardProduct.Footer price ={product.price}>Add To Cart</CardProduct.Footer>
-                </CardProduct>
-            ))}
-        
-        </div>
-    );
+  //code di bawah ada lah menghendle button logout
+  const handleLogout = () => {
+    localStorage.removeItem('email');
+    localStorage.removeItem('password');
+    window.location.href = '/login';
+  };
+
+  return (
+    <Fragment>
+      <div className="flex justify-end h-20 bg-blue-600 text-white px-10 items-center">
+        {email}
+        <Button classname="ml-5 bg-red-600" onclick={handleLogout}>
+          Logout
+        </Button>
+      </div>
+      <div className="flex justify-center py-5">
+        {products.map((product) => (
+          <CardProduct key={product.id}>
+            <CardProduct.Header image={product.image} />
+            <CardProduct.Body name={product.name}>{product.description}</CardProduct.Body>
+            <CardProduct.Footer price={product.price}>Add To Cart</CardProduct.Footer>
+          </CardProduct>
+        ))}
+      </div>
+    </Fragment>
+  );
 };
 
 export default ProductsPage;
--- src/components/Fragment/FormLogin.jsx
+++ src/components/Fragment/FormLogin.jsx
@@ -4,14 +4,20 @@ import Button from '../Elements/Button/Button';
 const FormLogin = () => {
   const handleLogin = (event) => {
     event.preventDefault();
-    console.log ("anda telah login");
+    // code local storage untuk menyimpat data ke local storage
+    localStorage.setItem('email', event.target.email.value);
+    localStorage.setItem('password', event.target.password.value);
+    //code di bawah ini untuk membawa ke halaman product setelah sukses login
+    window.location.href = '/products';
   };
 
   return (
     <form onSubmit={handleLogin}>
       <InputForm label="Email" type="email" placeholder="example@mail.com" name="email"></InputForm>
-      <InputForm label="Password" type="password" placeholder="******" name="passwprd" ></InputForm>
-      <Button classname="bg-blue-600 w-full" type="submit" >Login</Button>
+      <InputForm label="Password" type="password" placeholder="******" name="password"></InputForm>
+      <Button classname="bg-blue-600 w-full" type="submit" onClick={handleLogin}>
+        Login
+      </Button>
     </form>
   );
 };
