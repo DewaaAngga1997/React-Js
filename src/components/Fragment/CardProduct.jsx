2ec1f66df225e94faf0181fd74b484d1c78773fa
Dewa Angga <devaangga53@gmail.com>
2023-07-29 12:15:04 +0800

    hanya menambahkan komentar

--- src/components/Fragment/CardProduct.jsx
+++ src/components/Fragment/CardProduct.jsx
@@ -30,7 +30,8 @@ const Footer = (props) => {
   const { price, children } = props;
   return (
     <div className="flex items-center justify-between px-5 pb-5">
-      <span className="text-xl text-white font-bold">{price}</span>
+      <span className="text-xl text-white font-bold">Rp {price.toLocaleString('id-ID', { styles: 'currency', currency: 'IDR' })}</span>
+      {/* code {price.toLocaleString('id-ID', { styles: 'currency', currency: 'IDR' })} untuk format IDR titiknya */}
       <Button classname="bg-blue-600">{children}</Button>
     </div>
   );
