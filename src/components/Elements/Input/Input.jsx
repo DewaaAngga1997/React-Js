1a7b01a06f49d09c045dd37207002f8f4d83060c
Dewa Angga <devaangga53@gmail.com>
2023-08-02 13:27:31 +0800

    forwardRef

--- src/components/Elements/Input/Input.jsx
+++ src/components/Elements/Input/Input.jsx
@@ -1,5 +1,8 @@
-const Input = (props) => {
+import { forwardRef } from 'react';
+
+// forwardref bertujuan sama seperti props, jika menggunakan useRef dalam lomponen dan di panggil di komponen lain harus menggunakan forwardRef
+const Input = forwardRef((props, ref) => {
   const { type, placeholder, name } = props;
-  return <input type={type} className="text-sm border rounded w-full py-2 px-3 text-slate-700 placeholder:opacity-50" placeholder={placeholder} name={name} id={name} />;
-};
+  return <input type={type} className="text-sm border rounded w-full py-2 px-3 text-slate-700 placeholder:opacity-50" placeholder={placeholder} name={name} id={name} ref={ref} />;
+});
 export default Input;
