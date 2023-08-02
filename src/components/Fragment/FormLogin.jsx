82f5f1ad0cbf611a8ad319e310e384002cdb58ac
Dewa Angga <devaangga53@gmail.com>
2023-08-02 13:27:14 +0800

    membuat fungsi focus pada form login

--- src/components/Fragment/FormLogin.jsx
+++ src/components/Fragment/FormLogin.jsx
@@ -1,5 +1,6 @@
 import InputForm from '../Elements/Input/Index';
 import Button from '../Elements/Button/Button';
+import { useEffect, useRef } from 'react';
 
 const FormLogin = () => {
   // di bawah ini fungsi hendle login
@@ -12,9 +13,19 @@ const FormLogin = () => {
     window.location.href = '/products';
   };
 
+  //useRef untuk memanipulasi data DOM agar di form login kursor focus ke input email,
+  // supaya di halaman login kita bisa langsung mengetikan email, jadi tidak perlu lagi mengrahkan kursor ke input email dan mengklik kolom
+  //saat menggunakan useRef kita tidak bisa menggunakannya sama seperti props, maka dari itu kita harus menggunakan forwardRef
+  // karena InputForm kita memanggil komponennya menggunakan props maka di tiap komponennya kita tambahkan forwadRef di komponen Input.jsx dan Index.jsx
+  const emailRef = useRef(null);
+  useEffect(() => {
+    emailRef.current.focus();
+  }, []);
+
   return (
     <form onSubmit={handleLogin}>
-      <InputForm label="Email" type="email" placeholder="example@mail.com" name="email"></InputForm>
+      {/* tambahkan ref={emailRef} pada input email */}
+      <InputForm label="Email" type="email" placeholder="example@mail.com" name="email" ref={emailRef}></InputForm>
       <InputForm label="Password" type="password" placeholder="******" name="password"></InputForm>
       <Button classname="bg-blue-600 w-full" type="submit" onClick={() => handleLogin}>
         Login
