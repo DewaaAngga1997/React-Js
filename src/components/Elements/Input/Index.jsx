d6396820a844e2f6a444e7edc3ab41e3e8de1dec
Dewa Angga <devaangga53@gmail.com>
2023-08-02 13:27:35 +0800

    forwardRef

--- src/components/Elements/Input/Index.jsx
+++ src/components/Elements/Input/Index.jsx
@@ -1,14 +1,16 @@
 import Label from './Label';
 import Input from './Input';
+import { forwardRef } from 'react';
 
-const InputForm = (props) => {
+// forwardref bertujuan sama seperti props, jika menggunakan useRef dalam lomponen dan di panggil di komponen lain harus menggunakan forwardRef
+const InputForm = forwardRef((props, ref) => {
   const { label, name, type, placeholder } = props;
   return (
     <div className="mb-6">
       <Label htmlFor={name}>{label}</Label>
-      <Input name={name} type={type} placeholder={placeholder}></Input>
+      <Input name={name} type={type} placeholder={placeholder} ref={ref}></Input>
     </div>
   );
-};
+});
 
 export default InputForm;
